b7698a904f899d12cb051a7b41fb4dec1d8bc546
SameerFaisal <sameerfaisal46@gmail.com>
2023-05-08 09:07:04 +0500

    2210B1 JSON

---
+++ app.js
@@ -0,0 +1,18 @@
+// let firstName=prompt("What is your firstname?")
+// let lastName=prompt("What is your lastname?")
+
+// alert(`${firstName} ${lastName}`)
+
+// let names=["Mudassir","Talha","Shahood"]
+
+// for(let i=0;i<names.length;i++){
+//     console.log((i+1)+" : "+names[i])
+// }
+
+let obj={
+    stdName:"Ali",
+    age:20,
+    contact:[300,332]
+}
+
+alert(obj.contact[1])
